806c859536c457e66666390024273ca6aa5957ba
SzluDora <szlucska.dora@gmail.com>
2025-03-01 14:22:58 +0100

    adding functionality for editing todos

--- src/app/edit-todo/edit-todo.component.ts
+++ src/app/edit-todo/edit-todo.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LocalStorageService } from '../services/local-storage.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-todo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-todo.component.html',
-  styleUrl: './edit-todo.component.css'
+  styleUrl: './edit-todo.component.css',
 })
-export class EditTodoComponent {
+export class EditTodoComponent implements OnInit {
+  constructor(private localStorageService: LocalStorageService) {}
+  todos: string[] = [];
+  editedTodo: string = '';
+  editingIndex: number | null = null;
 
+  ngOnInit() {
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    const savedTodos = this.localStorageService.getItem<string[]>('todos');
+    if (savedTodos) {
+      this.todos = savedTodos;
+    }
+  }
+
+  startEditing(index: number) {
+    this.editingIndex = index;
+    this.editedTodo = this.todos[index];
+  }
+
+  saveEdit() {
+    if (this.editingIndex !== null && this.editedTodo.trim()) {
+      this.todos[this.editingIndex] = this.editedTodo;
+      this.localStorageService.setItem('todos', this.todos);
+      this.editingIndex = null;
+    }
+  }
+
+  cancelEdit() {
+    this.editingIndex = null;
+  }
 }
