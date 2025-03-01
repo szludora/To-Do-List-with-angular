02bb31b56c68f5f9e5cd666ec8e319fbff52dba4
SzluDora <szlucska.dora@gmail.com>
2025-03-01 13:23:54 +0100

    Setup navbar with dynamic component loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { HomeComponent } from './home/home.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { EditTodoComponent } from './edit-todo/edit-todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgbModule, NgbNavModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todo';
+  components = [HomeComponent, AddTodoComponent, EditTodoComponent];
 }
---
+++ src/app/edit-todo/edit-todo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-todo',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-todo.component.html',
+  styleUrl: './edit-todo.component.css'
+})
+export class EditTodoComponent {
+
+}
