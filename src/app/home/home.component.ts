3ef0fe659945479fb9eea542a9bf05a05f5d69f4
SzluDora <szlucska.dora@gmail.com>
2025-03-01 13:50:00 +0100

    add local storage support and test data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LocalStorageService } from '../services/local-storage.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit {
+  todos: string[] = [];
+  testTodos: string[] = ['Learn Angular', 'Build a project', 'Get a job offer'];
+
+  constructor(private localStorageService: LocalStorageService) {}
+
+  ngOnInit() {
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    const savedTodos = this.localStorageService.getItem<string[]>('todos');
+    if (savedTodos) {
+      this.todos = savedTodos;
+    }
+  }
+
+  addTestData() {
+    this.todos = [...this.todos, ...this.testTodos];
+    this.localStorageService.setItem('todos', this.todos);
+  }
 }
